352e7253b42a1b6d8aca41ad43a480eb9bbea0f9
Sharmander <jaimesmarlon497@gmail.com>
2026-08-18 07:37:55 -0500

    First commit using my gaming pc instead the one I use to bring to my university

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+Clase 1 - Introducción "Hola Mundo"
+video: https://www.youtube.com/watch?v=JOAqpdM36wI&t=3672s
+ */
+
+//Programa que ejecuta en terminal el mensaje Hola Mundo
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
